8b604a070c332cf597971192297d2b8a2c17fd6a
Loriasun <loriasun@umich.edu>
2023-12-05 22:59:08 -0600

    [fix] navbar

--- code/frontend/src/Navigation.js
+++ code/frontend/src/Navigation.js
@@ -13,6 +13,10 @@ const Navigation = () => {
                 <>
                     | <Link to="/report">Report Case</Link>
                     | <Link to="/search">Search Case</Link>
+                    | <Link to="/advancedSQL1">Advanced Query 1</Link>
+                    | <Link to="/advancedSQL2">Advanced Query 2</Link>
+                    | <Link to="/summary1">Summary 1</Link>
+                    | <Link to="/summary1">Summary 2</Link>
                     | <button onClick={auth.logOut}>Log Out</button>
                 </>
             ) : (
